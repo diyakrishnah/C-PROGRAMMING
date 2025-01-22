34b32a80caf08c53c55b26c0ffc1affb020a8129
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2025-01-22 16:26:01 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  printf("size of integer=%ld",sizeof(int));
+  printf("\nsize of long integer=%ld",sizeof(long int));
+  printf("\nsize of short integer=%ld",sizeof(short int));
+  printf("\nsize of float=%ld",sizeof(float ));
+  printf("\nsize of double=%ld",sizeof(double));
+  printf("\nsize of char=%ld",sizeof(char));
+  return 0;
+}
