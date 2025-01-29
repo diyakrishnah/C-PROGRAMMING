5f1d44b1432b9a48bca9a8485a7944a33a41872d
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2025-01-29 14:13:13 +0530

    Add files via upload

---
+++ Largest of 3 numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+  int number1,number2,number3;
+  printf("\nEnter three numbers");
+  scanf("%d%d%d",&number1,&number2,&number3);
+  if (number1>number2){
+        if (number1>number3){
+            printf("%d is the largest number",number1);
+      }
+  }
+  else if (number2>number3){
+        printf("%d is the largest number",number2);
+  }
+  else{
+        printf("%d is the largest number",number3);
+  }
+  return 0;
+}
+        
+  
+
