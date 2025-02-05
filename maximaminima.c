5aeded8eafe0e04e13ad8a8d0dce1dc1bab019b8
diyakrishnah <diyakrishnam2028@cs.sjcetpalai.ac.in>
2025-02-05 14:44:02 +0530

    Add files via upload

---
+++ maximaminima.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+  int i,limit,maxima,minima,max,mini;
+  printf("\nEnter the number of elements:");
+  scanf("%d",&limit);
+  int number[limit];
+  printf("\n Enter the %d elements",limit);
+  for (i=0;i<limit;i++){
+    scanf("%d",&number[i]);
+  }
+  maxima=number[0];
+  minima=number[0];
+  for(i=0;i<limit;i++){
+    if (number[i]>maxima){
+      maxima=number[i];
+      max=i+1;
+    }
+    if(number[i]<minima){
+      minima=number[i];
+      mini=i+1;
+    }
+  }
+  printf("maxima is %d ,at the position %d",maxima,max);
+  printf("\n minima is %d ,at the position %d",minima,mini);
+  return 0;
+}
+    
+    
